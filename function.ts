8a339e820311d6d6d722a0ee6d314b1a93ffbb80
Vaishnavi Santanse <santansesonali@gmail.com>
2019-02-08 22:56:40 +0530

    first commit

---
+++ function.ts
@@ -0,0 +1,29 @@
+
+function add(a: number , b:number)
+{
+    var c=a+b;
+return c;
+    
+}
+var z=add( 200,300);
+ console.log("addition is "+z)
+ 
+ function sub(a: number , b:number)
+ {
+     var c=a-b;
+ return c;
+     
+ }
+ var z=sub( 500,300);
+  console.log("substraction is "+z);
+
+function multiply(a: number , b:number)
+{
+    var c=a*b;
+return c;
+    
+}
+var z=multiply( 200,300);
+ console.log("Multiplication  is "+z)
+
+ 
